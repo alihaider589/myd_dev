d88240aa990f4373baaae3e8b5d2265d0716f7dd
alihaider589 <alihaider589@outlook.com>
2020-08-18 20:25:36 +0500

    adding initial components

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+
+import Block from "./Block";
+import Button from "./Button";
+import Input from "./Input";
+import Text from "./Text";
+import Card from './Card'
+import Divider from './Divider'
+import Badge from './Badge'
+export {Block, Button,Input,Text,Card,Divider,Badge};
